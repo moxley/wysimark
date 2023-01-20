e9a482c274e5c5c8f3724e108129bace99f9ed1d
Moxley Stratton <moxley.stratton@gmail.com>
2023-01-20 11:23:32 -0800

    Export parse function

--- packages/editor/editor/index.tsx
+++ packages/editor/editor/index.tsx
@@ -1,5 +1,6 @@
 export { Wysimark, React, ReactDOM } from "./wysimark"
 export { useEditor } from "./use-editor"
+export { parse } from "~/lib/convert/parse"
 
 /**
  * We export these types so that we can confirm that they match the types
